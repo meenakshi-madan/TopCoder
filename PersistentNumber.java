f600d602f3d38dbd0c40215edfc05e487c569a67
Meenakshi <profondeur@rocketmail.com>
2013-06-18 17:40:46 +0530

    Create PersistentNumber.java

---
+++ PersistentNumber.java
@@ -0,0 +1,132 @@
+import java.util.*;
+import java.util.regex.*;
+import java.text.*;
+import java.math.*;
+import java.awt.geom.*;
+
+public class PersistentNumber
+{
+  public int getPersistence(int n)
+	{
+		ArrayList<Integer> seq = new ArrayList<Integer>();
+		int prev = n;
+		seq.add(prev);
+		while(prev >= 10)
+		{
+			prev = getProduct(prev);
+			seq.add(prev);
+		}
+		
+		return (seq.size() - 1);
+	}
+	
+	private int getProduct(int x)
+	{
+		int product = 1, divisor = 10, rem=x;
+		while(rem > 0)
+		{
+			product *= rem%divisor;
+			rem /= 10;
+		}
+		
+		return product;
+	}
+	
+	public static void main(String[] args)
+	{
+		long time;
+		int answer;
+		boolean errors = false;
+		int desiredAnswer;
+		
+		
+		time = System.currentTimeMillis();
+		answer = new PersistentNumber().getPersistence(99);
+		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
+		desiredAnswer = 2;
+		System.out.println("Your answer:");
+		System.out.println("\t" + answer);
+		System.out.println("Desired answer:");
+		System.out.println("\t" + desiredAnswer);
+		if (answer != desiredAnswer)
+		{
+			errors = true;
+			System.out.println("DOESN'T MATCH!!!!");
+		}
+		else
+			System.out.println("Match :-)");
+		System.out.println();
+		time = System.currentTimeMillis();
+		answer = new PersistentNumber().getPersistence(268);
+		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
+		desiredAnswer = 4;
+		System.out.println("Your answer:");
+		System.out.println("\t" + answer);
+		System.out.println("Desired answer:");
+		System.out.println("\t" + desiredAnswer);
+		if (answer != desiredAnswer)
+		{
+			errors = true;
+			System.out.println("DOESN'T MATCH!!!!");
+		}
+		else
+			System.out.println("Match :-)");
+		System.out.println();
+		time = System.currentTimeMillis();
+		answer = new PersistentNumber().getPersistence(6);
+		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
+		desiredAnswer = 0;
+		System.out.println("Your answer:");
+		System.out.println("\t" + answer);
+		System.out.println("Desired answer:");
+		System.out.println("\t" + desiredAnswer);
+		if (answer != desiredAnswer)
+		{
+			errors = true;
+			System.out.println("DOESN'T MATCH!!!!");
+		}
+		else
+			System.out.println("Match :-)");
+		System.out.println();
+		time = System.currentTimeMillis();
+		answer = new PersistentNumber().getPersistence(68889789);
+		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
+		desiredAnswer = 3;
+		System.out.println("Your answer:");
+		System.out.println("\t" + answer);
+		System.out.println("Desired answer:");
+		System.out.println("\t" + desiredAnswer);
+		if (answer != desiredAnswer)
+		{
+			errors = true;
+			System.out.println("DOESN'T MATCH!!!!");
+		}
+		else
+			System.out.println("Match :-)");
+		System.out.println();
+		time = System.currentTimeMillis();
+		answer = new PersistentNumber().getPersistence(86898);
+		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
+		desiredAnswer = 7;
+		System.out.println("Your answer:");
+		System.out.println("\t" + answer);
+		System.out.println("Desired answer:");
+		System.out.println("\t" + desiredAnswer);
+		if (answer != desiredAnswer)
+		{
+			errors = true;
+			System.out.println("DOESN'T MATCH!!!!");
+		}
+		else
+			System.out.println("Match :-)");
+		System.out.println();
+		
+		
+		if (errors)
+			System.out.println("Some of the test cases had errors :-(");
+		else
+			System.out.println("You're a stud (at least on the test data)! :-D ");
+	}
+
+}
+//Powered by [KawigiEdit] 2.0!
